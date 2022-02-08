6cadb786346032a0e8d8a2f02a4a640f3c9b680d
RuthAdler <85292214+RuthAdler@users.noreply.github.com>
2022-02-08 21:15:33 +0200

    Chapter 1 Question 5

---
+++ Days.java
@@ -0,0 +1,7 @@
+public class Days {
+    public static void main(String [] args){
+        System.out.println("Days of the week:"); 
+        System.out.println("Sunday\nMonday\nTuesday\n");
+        
+    }
+}
